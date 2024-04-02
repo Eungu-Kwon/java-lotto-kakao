e815b9a9af83b338fe1d5b2a3d649f084869aa44
zero.1 <zero.1@kakaocorp.com>
2024-04-02 15:22:37 +0900

    feat: 금액에 맞는 로또 티켓을 생성하고 총 수익을 계산하는 클래스 구현

---
+++ src/main/java/com/lotto/model/LottoGame.java
@@ -0,0 +1,31 @@
+package com.lotto.model;
+
+import com.lotto.util.LottoGenerateStrategy;
+
+public class LottoGame {
+	private static final int LOTTO_PRICE = 1000;
+	private LottoTickets lottoTickets;
+	private LottoResults lottoResults;
+
+	public LottoGame(int money, LottoGenerateStrategy lottoGenerateStrategy) {
+		this.lottoTickets = new LottoTickets(money / LOTTO_PRICE, lottoGenerateStrategy);
+		this.lottoResults = new LottoResults();
+	}
+
+	public LottoTickets getLottoTickets() {
+		return lottoTickets;
+	}
+
+	public double calculateProfit() {
+		double totalPrize = lottoResults.calculateTotalPrize();
+		return totalPrize / (lottoTickets.size() * LOTTO_PRICE);
+	}
+
+	public LottoResults play(TargetLotto targetLotto) {
+		lottoTickets.getLottoTickets().forEach(lottoTicket -> {
+			LottoRank lottoRank = targetLotto.match(lottoTicket);
+			lottoResults.addLottoCount(lottoRank);
+		});
+		return lottoResults;
+	}
+}
